cdbf313ed62ec46189eafc2df6a220906a928659
Miladepour <epour.milad@gmail.com>
2022-11-21 09:49:43 +0000

    A pass test done

--- A-pass-tests/02-get-average/get-average.js
+++ A-pass-tests/02-get-average/get-average.js
@@ -2,6 +2,9 @@
 // return the average of all the numbers
 // be sure to exclude the strings
 
-function average(numbers) {}
+function average(numbers) {
+    let newArry = numbers.filter(num => typeof num === "number") 
+    return newArry.reduce((a,b) => {return a + b},0) / newArry.length
+  }
 
 module.exports = average;
--- A-pass-tests/09-car-sales/car-sales.js
+++ A-pass-tests/09-car-sales/car-sales.js
@@ -1,3 +1,12 @@
-function sales(carsSold) {}
-
+function sales(carsSold) {
+    let totals = {}
+      for(let car of carsSold) {
+        if (!totals[car.make]) {
+          totals[car.make] = car.price;
+        } else {
+          totals[car.make] += car.price;
+        }
+      }
+      return totals
+    }
 module.exports = sales;
